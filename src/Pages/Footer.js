4d46c6462b9fdb4948d1c9d4b95c5f943a666be4
mhk98 <mohsinkabirseo@gmail.com>
2022-07-28 22:08:35 +0600

    profile page final update

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,4 +1,4 @@
-import { faAngleRight, faPenToSquare, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faGear, faPenToSquare, faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
@@ -7,7 +7,7 @@ const Footer = () => {
         <div>
             <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 text-white py-8 mx-auto'>
             <div className='flex items-center justify-center'>
-                <FontAwesomeIcon className='mr-4 h-12 w-12 rounded-lg bg-white text-[#A3A7BC]' icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon className='mr-4 h-8 w-8 rounded-lg bg-white text-[#A3A7BC]' icon={faStar}></FontAwesomeIcon>
                 <span className='text-2xl' >FAQ</span>
             </div>
             <div>
@@ -17,14 +17,15 @@ const Footer = () => {
         </div>
             <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 text-white py-8 mx-auto'>
             <div className='flex items-center justify-center'>
-                <FontAwesomeIcon className='mr-4 h-12 w-12 rounded-lg bg-white text-[#A3A7BC]' icon={faStar}></FontAwesomeIcon>
-                <span className='text-2xl' >FAQ</span>
+                <FontAwesomeIcon className='mr-4 h-8 w-8 rounded-lg bg-white text-[#A3A7BC]' icon={faGear}></FontAwesomeIcon>
+                <span className='text-2xl' >Setting</span>
             </div>
             <div>
             <FontAwesomeIcon className='text-2xl' icon={faAngleRight}></FontAwesomeIcon>
             </div>
 
         </div>
+        <h1 className='text-2xl text-white py-8'>Unsubcribe</h1>
         </div>
     );
 };
